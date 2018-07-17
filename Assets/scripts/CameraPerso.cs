fdc28c0ae21b8aa76594a6d0fa15dd5ab9b65fa5
Unknown <natanjeunehomme@gmail.com>
2018-07-17 16:24:02 +0200

    deplacement perso

---
+++ Assets/scripts/CameraPerso.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+
+public class CameraPerso : MonoBehaviour
+{
+    #region Variables (public)
+
+    public float m_FDirectionDeSuivi = 0.0f;
+
+    #endregion
+
+    #region Variables (private)
+
+
+
+    #endregion
+
+    private void SuiviPerso()
+    {
+    
+    }
+
+
+}
--- Assets/scripts/PersonnageJoueur.cs
+++ Assets/scripts/PersonnageJoueur.cs
@@ -7,6 +7,7 @@ public class PersonnageJoueur : MonoBehaviour
 
    public int m_iHp = 100;
 
+    public float m_fVitesse = 1000.0f;
     #endregion
 
     #region Variables (private)
@@ -18,17 +19,26 @@ public class PersonnageJoueur : MonoBehaviour
 
     private void Update()
     {
-        
+        MovePersonnage();
     }
 
-    private void mouvpersonnage()
+    private void MovePersonnage()
     {
+         float FHorizontal = Input.GetAxis("Horizontal");
+         float FVertical = Input.GetAxis("Vertical");
 
+        Vector3 tDirection = new Vector3(FHorizontal, 0.0f, FVertical);
+
+        if (tDirection != Vector3.zero)
+        {
+
+            tDirection.Normalize();
+
+            Vector3 tDeplacement = tDirection * (m_fVitesse * Time.deltaTime); // deplacement de 1 en profondeur
+         transform.position += tDeplacement;
+
+            transform.forward = tDirection;
+
+        }
     }
-        
-
-
-
-
-}                                                                
-     
+}                                                               
