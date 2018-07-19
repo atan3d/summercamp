e376ac9b139b5764876a9ed43b32a4d71ccce0f6
Unknown <natanjeunehomme@gmail.com>
2018-07-19 17:08:20 +0200

    sauvegarde jeudi

---
+++ Assets/scripts/Personnages/Ennemi.cs
@@ -0,0 +1,30 @@
+﻿
+using UnityEngine;
+
+public class Ennemi : Personnage
+{
+    #region Variables (public)
+
+
+
+    #endregion
+
+    #region Variables (private)
+
+
+
+    #endregion
+
+
+
+
+    protected override void Attaquer()
+    {
+     
+    }
+
+    protected override void MovePersonnage()
+    {
+    
+    }
+}
---
+++ Assets/scripts/Personnages/PersonnageJoueur.cs
@@ -0,0 +1,76 @@
+﻿
+using UnityEngine;
+
+public class PersonnageJoueur : Personnage 
+{
+    #region Variables (public)
+
+    public Rigidbody m_pRigidbody = null;
+
+    public float m_fVitesseDeSaut = 1.0f;
+    #endregion
+
+    #region Variables (private)
+
+
+
+    #endregion
+
+   
+
+    private void Update()
+    {
+        Attaquer();
+        MovePersonnage();
+        Jump();
+    }
+
+    override protected void MovePersonnage()
+    {
+         float FHorizontal = Input.GetAxis("Horizontal");
+         float FVertical = Input.GetAxis("Vertical");
+
+        Vector3 tDirection = new Vector3(FHorizontal, 0.0f, FVertical);
+
+        if (tDirection != Vector3.zero)         
+        {
+
+           tDirection = CameraPerso.Instance.transform.TransformDirection(tDirection);
+            tDirection.y = 0.0f;
+
+            if (tDirection.sqrMagnitude != 0.0f)
+               tDirection.Normalize();
+            
+            else
+                tDirection = transform.forward;
+
+
+            Vector3 tDeplacement = tDirection * (m_fVitesse * Time.deltaTime); // deplacement de 1 en profondeur
+            m_pRigidbody.MovePosition(transform.position + tDeplacement);
+
+            transform.forward = tDirection;
+
+        }
+    }
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Vector3 tsaut = Vector3.up * (m_fVitesseDeSaut);
+            m_pRigidbody.AddForce(tsaut, ForceMode.Impulse);
+        
+        }
+    }
+
+
+    override protected void Attaquer()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            m_pArme.Attaquer();
+        
+
+        }
+    }
+
+}                                                               
